d87d5f5a0fd67a234ae0760c6d6c0a91e06c8223
jeaye <contact@jeaye.com>
2014-08-17 01:46:51 +0800

    Added new detail config

---
+++ include/jeayeson/detail/config.hpp
@@ -0,0 +1,26 @@
+/*
+  Copyright © 2014 Jesse 'Jeaye' Wilkerson
+  See licensing at:
+    http://opensource.org/licenses/BSD-3-Clause
+
+  File: detail/config.hpp
+  Author: Jesse 'Jeaye' Wilkerson
+*/
+
+#pragma once
+
+#include <cstdint>
+#include <map>
+
+/* XXX: This is not the file you're looking for. See config.hpp,
+ * not detail/config.hpp. Less important shit happens here. */
+
+namespace jeayeson
+{
+  template <typename T>
+  struct config;
+  struct config_tag
+  { };
+}
+
+#include "../config.hpp"
