45efda106eb9b9be06f98e848545596164160f64
jeaye <contact@jeaye.com>
2014-11-27 09:21:32 +0800

    Escape all required characters #9
    Tests will be needed for each of these; I'll break out into a separate
    'escape' file for testing all of them.

--- include/jeayeson/detail/escape.hpp
+++ include/jeayeson/detail/escape.hpp
@@ -10,17 +10,32 @@
 #pragma once
 
 #include <string>
+#include <algorithm>
 
 namespace jeayeson
 {
   namespace detail
   {
+    inline bool should_escape(char const c)
+    {
+      return c == '\"' || c == '\\' || c == '/' ||
+             c == '\b' || c == '\f' || c == '\n' ||
+             c == '\r' || c == '\t';
+    }
+
     inline std::string escape(std::string str)
     {
-      for(std::size_t start_pos{};
-          ((start_pos = str.find("\"", start_pos)) != std::string::npos);
-          start_pos += 2)
-      { str.replace(start_pos, 1, "\\\""); }
+      for(std::size_t i{}; i < str.size(); )
+      {
+        auto const found(std::find_if(std::next(str.begin(), i),
+                                      str.end(), &should_escape));
+        if(found == str.end())
+        { break; }
+
+        auto const dist(std::distance(str.begin(), found));
+        str.replace(dist, 1, "\\\"");
+        i = dist + 2;
+      }
       return str;
     }
   }
