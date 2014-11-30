d136ed35a2dfd80f396dba88146fcd502bd2979d
jeaye <contact@jeaye.com>
2014-11-30 13:49:26 +0800

    Add floating point parsing tests

--- test/include/parser/number.hpp
+++ test/include/parser/number.hpp
@@ -33,9 +33,49 @@ namespace jest
     expect_equal(arr.get<json_int>(2), 0);
   }
 
-  /* TODO: floating point */
   template <> template <>
-  void jeayeson::number_group::test<1>()
+  void jeayeson::number_group::test<1>() /* floating point */
   {
+    json_map const map{ R"raw({"foo":42.42, "bar":-193847.07, "zero":-0.0})raw" };
+    expect_almost_equal(map.get<json_float>("foo"), 42.42);
+    expect_almost_equal(map.get<json_float>("bar"), -193847.07);
+    expect_almost_equal(map.get<json_float>("zero"), 0.0);
+
+    json_array const arr{ R"raw([42.42, -193847.07, -0.0])raw" };
+    expect_almost_equal(arr.get<json_float>(0), 42.42);
+    expect_almost_equal(arr.get<json_float>(1), -193847.07);
+    expect_almost_equal(arr.get<json_float>(2), 0.0);
+  }
+
+  template <> template <>
+  void jeayeson::number_group::test<2>() /* exponential: e */
+  {
+    json_map const map
+    { R"raw({"foo":1.1e10, "bar":7.07e+15, "zero":2.123e-3})raw" };
+    expect_almost_equal(map.get<json_float>("foo"), 1.1e10);
+    expect_almost_equal(map.get<json_float>("bar"), 7.07e+15);
+    expect_almost_equal(map.get<json_float>("zero"), 2.123e-3);
+
+    json_array const arr
+    { R"raw([1.1e10, 7.07e+15, 2.123e-3])raw" };
+    expect_almost_equal(arr.get<json_float>(0), 1.1e10);
+    expect_almost_equal(arr.get<json_float>(1), 7.07e+15);
+    expect_almost_equal(arr.get<json_float>(2), 2.123e-3);
+  }
+
+  template <> template <>
+  void jeayeson::number_group::test<3>() /* exponential: E */
+  {
+    json_map const map
+    { R"raw({"foo":1.1E10, "bar":7.07E+15, "zero":2.123E-3})raw" };
+    expect_almost_equal(map.get<json_float>("foo"), 1.1E10);
+    expect_almost_equal(map.get<json_float>("bar"), 7.07E+15);
+    expect_almost_equal(map.get<json_float>("zero"), 2.123E-3);
+
+    json_array const arr
+    { R"raw([1.1E10, 7.07E+15, 2.123E-3])raw" };
+    expect_almost_equal(arr.get<json_float>(0), 1.1E10);
+    expect_almost_equal(arr.get<json_float>(1), 7.07E+15);
+    expect_almost_equal(arr.get<json_float>(2), 2.123E-3);
   }
 }
