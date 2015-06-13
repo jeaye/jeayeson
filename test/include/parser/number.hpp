4bbde41d559a89cc96ae0be3960cdf0f49d2fc40
jeaye <contact@jeaye.com>
2015-06-13 17:34:09 +0800

    Fix parser number tests

--- test/include/parser/number.hpp
+++ test/include/parser/number.hpp
@@ -27,7 +27,7 @@ namespace jest
     expect_equal(map.get<json_int>("bar"), -193847);
     expect_equal(map.get<json_int>("zero"), 0);
 
-    json_array const arr{ R"raw([42, -193847, 0])raw" };
+    json_array const arr{ json_data{ R"raw([42, -193847, 0])raw" } };
     expect_equal(arr.get<json_int>(0), 42);
     expect_equal(arr.get<json_int>(1), -193847);
     expect_equal(arr.get<json_int>(2), 0);
@@ -41,7 +41,7 @@ namespace jest
     expect_almost_equal(map.get<json_float>("bar"), -193847.07);
     expect_almost_equal(map.get<json_float>("zero"), 0.0);
 
-    json_array const arr{ R"raw([42.42, -193847.07, -0.0])raw" };
+    json_array const arr{ json_data{ R"raw([42.42, -193847.07, -0.0])raw" } };
     expect_almost_equal(arr.get<json_float>(0), 42.42);
     expect_almost_equal(arr.get<json_float>(1), -193847.07);
     expect_almost_equal(arr.get<json_float>(2), 0.0);
@@ -57,7 +57,7 @@ namespace jest
     expect_almost_equal(map.get<json_float>("zero"), 2.123e-3);
 
     json_array const arr
-    { R"raw([1.1e10, 7.07e+15, 2.123e-3])raw" };
+    { json_data{ R"raw([1.1e10, 7.07e+15, 2.123e-3])raw" } };
     expect_almost_equal(arr.get<json_float>(0), 1.1e10);
     expect_almost_equal(arr.get<json_float>(1), 7.07e+15);
     expect_almost_equal(arr.get<json_float>(2), 2.123e-3);
@@ -73,7 +73,7 @@ namespace jest
     expect_almost_equal(map.get<json_float>("zero"), 2.123E-3);
 
     json_array const arr
-    { R"raw([1.1E10, 7.07E+15, 2.123E-3])raw" };
+    { json_data{ R"raw([1.1E10, 7.07E+15, 2.123E-3])raw" } };
     expect_almost_equal(arr.get<json_float>(0), 1.1E10);
     expect_almost_equal(arr.get<json_float>(1), 7.07E+15);
     expect_almost_equal(arr.get<json_float>(2), 2.123E-3);
