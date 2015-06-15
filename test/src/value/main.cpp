2e80bf6dc62af6311588d4407e3a6f41693dcc60
jeaye <contact@jeaye.com>
2015-06-15 21:01:36 +0800

    Add the start of a new value test suite

---
+++ test/src/value/main.cpp
@@ -0,0 +1,19 @@
+/*
+  Copyright © 2015 Jesse 'Jeaye' Wilkerson
+  See licensing at:
+    http://opensource.org/licenses/BSD-3-Clause
+
+  File: test/src/value/main.cpp
+  Author: Jesse 'Jeaye' Wilkerson
+*/
+
+#include <jeayeson/jeayeson.hpp>
+#include <jest/jest.hpp>
+
+#include "map/ctor.hpp"
+
+int main()
+{
+  jest::worker const j{};
+  return j();
+}
